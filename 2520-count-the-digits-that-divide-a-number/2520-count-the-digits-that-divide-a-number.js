c0f7d8831a29a7a67e0b94876aaf71c077422746
Subrata Das <158165979+subratadasGit@users.noreply.github.com>
2026-05-27 21:55:02 +0530

    Time: 0 ms (100%), Space: 53 MB (87.77%) - LeetHub

--- 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.js
+++ 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.js
@@ -3,10 +3,10 @@
  * @return {number}
  */
 var countDigits = function(num) {
-    let arrNum = String(num).split("")
+    let arrNum = String(num).split("") // first of all you need to convert this into String because you need to use split("") function 
    let count = 0 ;
     for(let i = 0 ; i < arrNum.length ; i++){
-        if(num %  arrNum[i] == 0 ){
+        if(num % arrNum[i] == 0 ){ // check if  number is divisible by numbers in the arrNum then increase the count++
             count++
         }
     }
