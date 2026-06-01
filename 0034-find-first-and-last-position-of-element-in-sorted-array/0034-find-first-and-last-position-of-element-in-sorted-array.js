34a6c0e768e4cd6f65c0e0b8348033fe0f8f9b5c
Subrata Das <158165979+subratadasGit@users.noreply.github.com>
2026-06-01 22:29:36 +0530

    Time: 2 ms (9.2%), Space: 55.3 MB (18.68%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+    let first = -1;
+    let last = -1;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === target) {
+            if (first === -1) {
+                first = i;
+            }
+            last = i;
+        }
+    }
+
+    return [first, last];
+};
